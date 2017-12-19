0ee4db55f77c94e6600d9f242399ef4a8f65fe75
Hà Nguyễn Hoàng <hoangha@vp9.vn>
2017-12-19 09:41:55 +0700

    Ha tests pushing

--- openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/ryu/Documents/Project/AndroidProject/HighspeedLaserShooter/openCVLibrary331/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/ha/Working/HighSpeedLaserBeam/openCVLibrary331/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
